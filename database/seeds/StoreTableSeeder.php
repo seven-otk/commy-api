f1b10d6a9d7ea957b10b76f5ab2d1dbf630beeca
SevenOtk <seven_otk@outlook.com>
2018-08-12 18:39:31 +0100

    feat: adding database seeding

---
+++ database/seeds/StoreTableSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class StoreTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Models\Store::class, 10)->create()->each(function ($store) {
+            factory(App\Models\Customer::class, rand(10, 50))->create([
+               'store_id' => $store->id
+            ]);
+
+            $categories = factory(App\Models\ProductCategory::class, rand(1, 5))->create([
+               'store_id' => $store->id
+            ]);
+
+            $domains = factory(App\Models\StoreDomain::class, rand(1, 3))->create([
+                'store_id' => $store->id
+            ]);
+
+            $domains->each(function ($domain) use ($store, $categories) {
+                $locations = factory(App\Models\StoreLocation::class)->create([
+                    'store_id' => $store->id,
+                    'store_domain_id' => $domain->id
+                ]);
+
+                $locations->each(function ($location) use ($store, $categories) {
+
+                    $categories->each(function ($category) use ($store, $location) {
+                        factory(App\Models\Product::class, rand(1, 100))->create([
+                            'store_id' => $store->id,
+                            'store_location_id' => $location->id,
+                            'category_id' => $category->id,
+                            'sku' => '00000' . rand(0, 999)
+                        ]);
+                    });
+                });
+            });
+        });
+    }
+}
