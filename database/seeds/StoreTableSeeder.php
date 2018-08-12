f580ca9da371f70785513d71f6231c47fe92c9a6
SevenOtk <seven_otk@outlook.com>
2018-08-12 18:52:18 +0100

    refactor: revert back

--- database/seeds/StoreTableSeeder.php
+++ database/seeds/StoreTableSeeder.php
@@ -12,12 +12,17 @@ class StoreTableSeeder extends Seeder
     public function run()
     {
         factory(App\Models\Store::class, 10)->create()->each(function ($store) {
-            $customers = $this->createCustomerRows($store);
-            $categories = $this->createProductCategoriesRows($store);
-            $domains = $this->createDomainsRows($store);
+            $this->createCustomerRows($store);
+
+            $categories = factory(App\Models\ProductCategory::class, rand(1, 5))->create([
+               'store_id' => $store->id
+            ]);
+
+            $domains = factory(App\Models\StoreDomain::class, rand(1, 3))->create([
+                'store_id' => $store->id
+            ]);
 
             $domains->each(function ($domain) use ($store, $categories) {
-
                 $locations = factory(App\Models\StoreLocation::class)->create([
                     'store_id' => $store->id,
                     'store_domain_id' => $domain->id
@@ -26,59 +31,24 @@ class StoreTableSeeder extends Seeder
                 $locations->each(function ($location) use ($store, $categories) {
 
                     $categories->each(function ($category) use ($store, $location) {
-
-                        $products = factory(App\Models\Product::class, rand(1, 100))->create([
+                        factory(App\Models\Product::class, rand(1, 100))->create([
                             'store_id' => $store->id,
                             'store_location_id' => $location->id,
                             'category_id' => $category->id,
-                        ]);
-
-                        $products->each(function ($product) {
+                        ])->each(function ($product) {
                             $product->update([
                                 'sku' => '00000' . rand(0, 999)
                             ]);
                         });
                     });
-
                 });
             });
         });
     }
 
-    /**
-     * Create the customer rows associated to the store.
-     *
-     * @param $store
-     */
     protected function createCustomerRows($store)
     {
         factory(App\Models\Customer::class, rand(10, 50))->create([
-            'store_id' => $store->id
-        ]);
-    }
-
-    /**
-     * Create the product rows associated with the store.
-     *
-     * TODO: Add in store locations?
-     *
-     * @param $store
-     */
-    protected function createProductCategoriesRows($store)
-    {
-        factory(App\Models\ProductCategory::class, rand(1, 5))->create([
-            'store_id' => $store->id
-        ]);
-    }
-
-    /**
-     * Create the domains row associated to the store
-     *
-     * @param $store
-     */
-    protected function createDomainsRows($store)
-    {
-        factory(App\Models\ProductCategory::class, rand(1, 5))->create([
             'store_id' => $store->id
         ]);
     }
