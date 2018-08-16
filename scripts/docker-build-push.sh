ad1e437add8fccdfb9ea1887f3dddd1bfcfd2e23
Connor Miotk <seven_otk@outlook.com>
2018-08-16 14:39:35 +0100

    fix: take out branch names that aren't needed

--- scripts/docker-build-push.sh
+++ scripts/docker-build-push.sh
@@ -4,12 +4,8 @@ set -ev
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker build -t $IMAGE_NAME -f docker/api-dockerfile .
+docker build -t $IMAGE_NAME:${TRAVIS_COMMIT:0:7} -f docker/api-dockerfile .
 
-docker tag $IMAGE_NAME:$TRAVIS_BRANCH;
-docker push $IMAGE_NAME:$TRAVIS_BRANCH;
-
-docker tag $IMAGE_NAME:${TRAVIS_COMMIT:0:7};
 docker push $IMAGE_NAME:${TRAVIS_COMMIT:0:7};
 
 if [ ${TRAVIS_BRANCH} = "master" ]; then
